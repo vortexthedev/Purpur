6b6c7b10adaae0ddd142573f4913b22415060e6b
xVorttex <sigamatute@gmail.com>
2024-09-25 18:07:29 +0300

    Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,8 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     publishing.repositories.maven("https://repo.purpurmc.org/snapshots") {
